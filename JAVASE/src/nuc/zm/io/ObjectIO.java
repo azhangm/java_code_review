5016bb07b648dd9ba5538b4444d97ef9e2b9061b
da juancai[2~ <742824624@qq.com>
2022-10-13 12:58:07 +0800

    对象的序列化和反序列化练习

--- JAVASE/src/nuc/zm/io/ObjectIO.java
+++ JAVASE/src/nuc/zm/io/ObjectIO.java
@@ -1,5 +1,9 @@
 package nuc.zm.io;
 
+import org.junit.Test;
+
+import java.io.*;
+
 /**
  * 对象输入输出
  *
@@ -8,4 +12,22 @@ package nuc.zm.io;
  */
 public class ObjectIO {
 
+    /**
+     * 测试字符串序列化
+     */
+    @Test
+    public void testStringSerialization() throws IOException {
+        File file = new File("demo.txt");
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
+        oos.writeObject("你好");
+        oos.close();
+    }
+
+    @Test
+    public void testStringDeSerialization() throws IOException, ClassNotFoundException {
+        File file = new File("demo.txt");
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file));
+        Object o = objectInputStream.readObject();
+        System.out.println(((String) o));
+    }
 }
---
+++ JAVASE/src/nuc/zm/io/ObjectIOTest/MyObject.java
@@ -0,0 +1,72 @@
+package nuc.zm.io.ObjectIOTest;
+
+import org.junit.Test;
+
+import java.io.*;
+
+public class MyObject {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Person person = new Person( 10 ,"nuc_zzmm");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("demo1.txt"));
+        objectOutputStream.writeObject(person);
+        objectOutputStream.close();
+
+        File file = new File("demo1.txt");
+//
+        Person person1 = deSerialize(file);
+        System.out.println(person1.age);
+        System.out.println(person1.name);
+    }
+
+
+
+//  标识接口 （什么都没有的接口）
+    private static class Person implements Serializable {
+        public static final long serialVersionUID = 1312312313L;
+
+        int age;
+        String name;
+
+        public int getAge() {
+            return age;
+        }
+
+//        public void setAge(int age) {
+//            this.age = age;
+//        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+
+        public Person() {
+        }
+
+        public Person(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
+
+    public static Person deSerialize(File file) throws IOException, ClassNotFoundException {
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+        Person person = (Person) ois.readObject();
+        ois.close();
+        return person;
+    }
+}
