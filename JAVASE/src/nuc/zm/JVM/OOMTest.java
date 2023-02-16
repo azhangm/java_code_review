a2ace6e0e4b1e6557a078bebf818c1bb6d8a2092
da juancai[2~ <742824624@qq.com>
2023-02-16 18:22:18 +0800

    :tada: 堆空间异常测试。  VisualVM 可视化工具的使用

---
+++ JAVASE/src/nuc/zm/JVM/OOMTest.java
@@ -0,0 +1,28 @@
+package nuc.zm.JVM;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *  oomtest
+ * @description  ： 堆空间 oom 异常的演示
+ * @author zm
+ * @date 2023/02/16
+ */
+public class OOMTest {
+    public static void main(String[] args) throws InterruptedException {
+        ArrayList<Ob> objects = new ArrayList<>();
+        while (true) {
+            Thread.sleep(200 );
+            objects.add(new Ob(1024*1024));
+        }
+    }
+
+    static class Ob {
+        int[] a;
+
+        public Ob(int len) {
+            a = new int[len];
+        }
+    }
+}
