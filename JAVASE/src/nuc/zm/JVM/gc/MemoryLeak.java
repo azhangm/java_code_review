5c5957b2bbf9d2c8314db94cfd8503bb1149089f
da juancai[2~ <742824624@qq.com>
2023-02-20 20:58:03 +0800

    :tada: 互相引用计数不为0 但是被回收了 说明默认 垃圾判断算法不是引用计数算法

---
+++ JAVASE/src/nuc/zm/JVM/gc/MemoryLeak.java
@@ -0,0 +1,30 @@
+package nuc.zm.JVM.gc;
+
+/**
+ * 内存泄漏
+ * @description : 演示内存泄漏问题
+ * @author zm
+ * @date 2023/02/20
+ */
+public class MemoryLeak {
+
+    private byte[] bigMemory = new byte[5 * 1024 * 1024]; // 5m
+
+    Object reference = null;
+
+    public static void main(String[] args) {
+        MemoryLeak memoryLeak = new MemoryLeak();
+        MemoryLeak memoryLeak1 = new MemoryLeak();
+        memoryLeak.reference = memoryLeak1;
+//        Variable is already assigned to this value 变量已经分配给这个值。
+        memoryLeak1.reference = memoryLeak;
+
+        System.gc();
+
+        try {
+            Thread.sleep(100000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
